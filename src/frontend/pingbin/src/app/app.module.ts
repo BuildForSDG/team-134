ab97f13b4d2b97539264dd79c0e55c05b04e1122
Makavura <makavuramughanga@gmail.com>
2020-05-10 17:17:05 +0300

    chore: redirect default path to landing page

--- src/frontend/pingbin/src/app/app.module.ts
+++ src/frontend/pingbin/src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: LandingPageComponent
+      }
+    ])
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
